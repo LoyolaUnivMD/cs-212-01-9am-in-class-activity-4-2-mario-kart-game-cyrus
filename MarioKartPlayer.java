03cb52e1a820d2755223ebcfd34f8053dde4392b
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-02-08 16:50:36 -0500

    Create MarioKartPlayer.java

---
+++ MarioKartPlayer.java
@@ -0,0 +1,27 @@
+public class MarioKartPlayer {
+  private String playerName;
+  private String characterName;
+  private int characterSpeed;
+
+  public MarioKartPlayer(){
+    playerName = "undefined";
+    characterName = "undefined";
+    characterSpeed = 0;
+  }
+
+  public MarioKartPlayer(String playerName, String characterName, int characterSpeed){
+    this.playerName = playerName;
+    this.characterName = characterName;
+    this.characterSpeed = 0;
+  }
+
+  public void boostSpeed(int boostAmount){
+    speed += (characterSpeed + boostAmount <= 100)? boostAmount : 0;
+  }
+
+  public void displayInfo(){
+    System.out.println("Player: " + playerName +
+                       "\nCharacter: " + characterName +
+                       "\nSpeed: " + characterSpeed);
+  }
+}
